922d3ec12b8684d9fc0857ae5a5ea678894b3937
Thomas Keil <thomas@weblizards.de>
2019-07-08 20:01:18 +0200

    * Removed unnecessary stuff * Added documentation

--- src/devices/renkforce_aok5055.c
+++ src/devices/renkforce_aok5055.c
@@ -1,40 +1,46 @@
 
-#include "decoder.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/*
- * Hypothetical template device
- *
- * Message is 68 bits long
- * Messages start with 0xAA
- * The message is repeated as 5 packets,
- * require at least 3 repeated packets.
- *
+/**
+ * Renforce AOK-5055 protocol.
+ * 
+ * The analyzation and documentation of the packages was done by https://github.com/Rotzbua and 
+ * is documented here: https://github.com/Rotzbua/reverse_engineering_weatherstation_conrad#package-format
+ * 
+ * The sensor sends 96 bits in 24 nibbles.
+ * Format:
+ * PPPPPPRRBTTTHHVVVSSDCCpp
+ * 
+ * P: Preamble, always aaa598
+ * R: Random ID that changes every time the battery is changed
+ * B: Battery status
+ * T: Temperature in °C
+ * H: Humidity
+ * V: Rain volume
+ * S: Wind speed
+ * D: Wind direction
+ * C: Checksum
+ * p: Pause
+ * 
+ * Example: aaa5980f00905305e02da380
+ * 
  */
-const char* hexString(unsigned char *bytes, int bytesLength) 
-{ 
-    char lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' }; 
-    char *s = malloc(sizeof(char) * (bytesLength * 2 + 1)); 
-    for (int i = 0; i < bytesLength; i++) { 
-        s[i*2] =       lookup[(bytes[i] >> 4) & 0xF]; 
-        s[i * 2 + 1] = lookup[bytes[i] & 0xF]; 
-    } 
-    s[bytesLength * 2] = 0;
-    return s; 
-}
+#include "decoder.h"
 
-#define AOK5055_BITLEN      96
-#define AOK5055_STARTBYTE   0xAA
+// The lenght of a message in bits
+#define AOK5055_MESSAGE_BITLEN      12 * 8
+
+// The lenght of the preamble in bits
+#define AOK5055_MESSAGE_PREAMBLELEN  3 * 8
+
+// How often needs the message to be repeated 
 #define AOK5055_MINREPEATS  3
 
-#define MYDEVICE_MSG_TYPE    0x10
-#define MYDEVICE_CRC_POLY    0x07
-#define MYDEVICE_CRC_INIT    0x00
+#define AOK5055_MILLIMETER_PER_STEP 0.75
 
-
+// The preamble of the message. These re the nibbles 0xaaa598
 static unsigned char preamble[3] = { 0xaa, 0xa5, 0x98 };
+
+// A lookup table to convert the direction-nibble (thus sixteen values)
+// to a nice direction.
 static unsigned char direction_lookup[16][4] = {
         "  N",
         "NNO",
@@ -58,35 +64,29 @@ static int renkforce_aok5055_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     data_t *data;
 
-	bitrow_t *bb = bitbuffer->bb;
-	unsigned bitpos = 0;
-	unsigned bits = bitbuffer->bits_per_row[0];
     unsigned char bytes[18]; // aligned packet data
-
-    // 24 Nibbles
-    // aaa598 0f00905305e02da380
     
     bitbuffer_invert(bitbuffer);
 
-	// Search for only 22 bits to cope with inverted frames and
-	// the missing final preamble bit with RFM69 transmissions.
-	bitpos = bitbuffer_search(bitbuffer, 0, bitpos, preamble, 24);
-
+    // Check if the preamble 0xaaa598 is found at all
+	unsigned int bitpos = bitbuffer_search(bitbuffer, 0, 0, preamble, AOK5055_MESSAGE_PREAMBLELEN);
     if (bitpos == bitbuffer->bits_per_row[0]) {
+        // Not found
         return 0;
     }
 
-    bitbuffer_extract_bytes(bitbuffer, 0, bitpos, bytes, 12 * 8);
+
+    bitbuffer_extract_bytes(bitbuffer, 0, bitpos, bytes, AOK5055_MESSAGE_BITLEN);
 
     int humidity = bytes[6];
     int temperature_c = ((bytes[4] & 0x0f) << 8) | bytes[5];
     int rain_steps = ((bytes[7]) << 4) | ((bytes[8]) >> 4);
     float temperature = temperature_c / 10;
     int wind_speed = ((bytes[8] & 0x0f) << 8) | (bytes[9] >> 4);
-    double rain_mm = rain_steps * 0.75;
+    double rain_mm = rain_steps * AOK5055_MILLIMETER_PER_STEP;
     int wind_direction = (bytes[9] & 0x0f);
-    double wind_degrees = wind_direction * 22.5; 
-
+    double wind_degrees = wind_direction * 22.5;
+    uint8_t battery = (bytes[4] & 0xf0) == 0xf0; 
 
     data = data_make(
             "model", "", DATA_STRING, "Renkforce AOK5055",
@@ -96,8 +96,7 @@ static int renkforce_aok5055_callback(r_device *decoder, bitbuffer_t *bitbuffer)
             "wind_degrees", "Wind degrees", DATA_FORMAT, "%.1f °", DATA_DOUBLE, wind_degrees,
             "wind_speed", "Wind speed", DATA_FORMAT, "%u km/h", DATA_INT, wind_speed,
             "rain_volume", "Rain volume", DATA_FORMAT, "%.1f mm", DATA_DOUBLE, rain_mm,
-            "battery", "Battery", DATA_INT, 1,
-            "raw", "Raw", DATA_STRING, hexString(bytes, 12),
+            "battery", "Battery", DATA_STRING, battery ? "LOW" : "OK",
             NULL);
 
     decoder_output_data(decoder, data);
@@ -112,7 +111,6 @@ static char *renkforce_aok5055_output_fields[] = {
     "wind_speed",
     "rain_volume",
     "battery",
-    "raw",
     NULL
 };
 
